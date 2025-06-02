7671428f48f4ff34584411cc7aaa6505439f3f1e
abd <abdamapro22@gmail.com>
2025-06-02 19:33:31 +0300

    added server side logic

--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
@@ -1,9 +1,23 @@
 # Copyright (c) 2025, abood-ama and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SalesInvoice(Document):
-	pass
+    def before_save(self):
+        self.calculate_totals()
+
+    def calculate_totals(self):
+        total_qty = 0
+        total_amount = 0
+        for item_row in self.get("items"): 
+            
+            item_row.amount = flt(item_row.quantity) * flt(item_row.rate) 
+
+            total_qty += float(item_row.quantity or 0)
+            total_amount += float(item_row.amount or 0)
+
+        self.total_qty = total_qty
+        self.total_amount = total_amount
