7838e0f890067f7f89e2e898a0a67484050ae822
abd <abdamapro22@gmail.com>
2025-06-02 18:59:57 +0300

    created purchase invoce and its child for items also did the logic for the total amount calculations

---
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.js
@@ -0,0 +1,35 @@
+
+frappe.ui.form.on('Purchase Invoice', {
+    calculate_totals: function(frm) {
+        let total_qty = 0;
+        let total_amount = 0;
+        if (frm.doc.items) {
+            frm.doc.items.forEach(function(item_row) {
+                total_qty += flt(item_row.quantity);
+                total_amount += flt(item_row.amount);
+            });
+        }
+        frm.set_value('total_qty', total_qty);
+        frm.set_value('total_amount', total_amount);
+        frm.refresh_field('total_qty');
+        frm.refresh_field('total_amount');
+    }
+});
+
+frappe.ui.form.on('Purchase Invoice Item', {
+    qty: function(frm, cdt, cdn) {
+        let item_row = locals[cdt][cdn];
+        item_row.amount = flt(item_row.quantity) * flt(item_row.rate);
+        frm.refresh_field('items');
+        frm.events.calculate_totals(frm);
+    },
+    rate: function(frm, cdt, cdn) {
+        let item_row = locals[cdt][cdn];
+        item_row.amount = flt(item_row.quantity) * flt(item_row.rate);
+        frm.refresh_field('items');
+        frm.events.calculate_totals(frm);
+    },
+    items_remove: function(frm) {
+        frm.events.calculate_totals(frm);
+    }
+});
