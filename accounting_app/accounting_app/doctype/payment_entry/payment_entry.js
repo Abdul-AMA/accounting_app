e9212add5477bb2a0eba134cb29f036e85a6957c
Abdul-AMA <abdamapro22@gmail.com>
2025-06-15 14:34:05 +0300

    feat: fix refresh loop

--- accounting_app/accounting_app/doctype/payment_entry/payment_entry.js
+++ accounting_app/accounting_app/doctype/payment_entry/payment_entry.js
@@ -1,5 +1,6 @@
+
 frappe.ui.form.on('Payment Entry', {
-    refresh: function(frm) {
+    on_load: function(frm) {
         frm.trigger('setup_dynamic_fields');
     },
 
@@ -13,33 +14,32 @@ frappe.ui.form.on('Payment Entry', {
         const payment_type = frm.doc.payment_type;
 
         if (payment_type === 'Receive') {
-            frm.get_field('account_paid_from').set_label('Received From Account');
-            frm.get_field('account_paid_to').set_label('Deposited To Account'); 
+            frm.get_field('account_paid_from').set_label('Received From Account'); 
+            frm.get_field('account_paid_to').set_label('Deposited To Account');  
         } else if (payment_type === 'Pay') {
-            frm.get_field('account_paid_from').set_label('Paid From Account');     
-            frm.get_field('account_paid_to').set_label('Paid To Account');    
+            frm.get_field('account_paid_from').set_label('Paid From Account');   
+            frm.get_field('account_paid_to').set_label('Paid To Account');      
         } else {
-           
             frm.get_field('account_paid_from').set_label('Account paid from');
             frm.get_field('account_paid_to').set_label('Account Paid to');
         }
         frm.refresh(); 
+
 
         frm.set_query('account_paid_from', function() {
             let filters = {'is_group': 0}; 
             if (payment_type === 'Receive') {
                 filters['account_type'] = 'Asset';
             } else if (payment_type === 'Pay') {
-               filters['account_type'] = 'Asset';
+                filters['account_type'] = 'Asset';
             }
             return { filters: filters };
         });
 
-        // Filter for the target account
         frm.set_query('account_paid_to', function() {
-            let filters = {'is_group': 0};
-		if (payment_type === 'Receive') {
-               filters['account_type'] = 'Asset';
+            let filters = {'is_group': 0}; 
+            if (payment_type === 'Receive') {
+                filters['account_type'] = 'Asset';
             } else if (payment_type === 'Pay') {
                 filters['account_type'] = 'Liability';
             }
