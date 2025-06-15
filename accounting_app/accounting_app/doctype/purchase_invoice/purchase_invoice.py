cc3857d7e428f075501d30ced49760ec8f13af57
Abdul-AMA <abdamapro22@gmail.com>
2025-06-15 12:42:20 +0300

    feat: Add backend total calculations for Purchase Invoice

--- accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.py
+++ accounting_app/accounting_app/doctype/purchase_invoice/purchase_invoice.py
@@ -1,9 +1,18 @@
-# Copyright (c) 2025, abood-ama and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class PurchaseInvoice(Document):
-	pass
+    # This method is automatically called by Frappe just before saving the document
+    def before_save(self):
+        self.calculate_totals()
+
+    def calculate_totals(self):
+        
+        total_qty = 0
+        total_amount = 0
+        for item_row in self.get("items"):
+            total_qty += float(item_row.qty or 0)
+            total_amount += float(item_row.amount or 0)
+
+        self.total_qty = total_qty
+        self.total_amount = total_amount
