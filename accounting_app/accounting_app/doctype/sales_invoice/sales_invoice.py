c76a276832aeafccc72d1de8ed27d40bcf126417
Abdul-AMA <abdamapro22@gmail.com>
2025-06-16 16:51:55 +0300

    feat: Implement immutable ledger as a perant class
    - inherated the class in sales invoice
    - updated version

--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
@@ -1,34 +1,36 @@
-import frappe
-from frappe.model.document import Document
-from utils.gl_entries_control import create_gl_entry,reverse_gl_entries
+from accounting_app.accounting_app.controllers.accounting_controller import AccountingController
 
-class SalesInvoice(Document):
+class SalesInvoice(AccountingController):
+    
     def before_save(self):
         self.calculate_totals()
 
+    def make_gl_entries(self):
 
-    def on_submit(self):
-        # Debit Entry
-        create_gl_entry(self.posting_date,self.debit_to,self.customer,self.total_amount,0,self.doctype,self.name,self.payment_due_date ,None)
+        #debit entry
+        self._create_gl_entry(
+            posting_date=self.posting_date,
+            account=self.debit_to,
+            party=self.customer,
+            debit=self.total_amount,
+            credit=0,
+            due_date=self.payment_due_date
+        )
 
-
-        # Credit Entry
-        create_gl_entry(self.posting_date,self.income_account,self.customer,0,self.total_amount,self.doctype,self.name, None ,None)
-
-
-    def on_cancel(self):
-        #create reverse entry
-        reverse_gl_entries(self.doctype,self.name,self.posting_date)
-
- 
+        #credit entry
+        self._create_gl_entry(
+            posting_date=self.posting_date,
+            account=self.income_account,
+            party=self.customer,
+            debit=0,
+            credit=self.total_amount
+        )
 
     def calculate_totals(self):
-        
         total_qty = 0
         total_amount = 0
         for item_row in self.get("items"):
             total_qty += float(item_row.qty or 0)
             total_amount += float(item_row.amount or 0)
-
         self.total_qty = total_qty
         self.total_amount = total_amount
