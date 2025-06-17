f287d985ce791ed454baf7d181ad318bcc661350
Abdul-AMA <abdamapro22@gmail.com>
2025-06-17 10:24:32 +0300

    feat:Implemented GL for Journal Entry by inheritance and update version

--- accounting_app/accounting_app/doctype/journal_entry/journal_entry.py
+++ accounting_app/accounting_app/doctype/journal_entry/journal_entry.py
@@ -1,8 +1,24 @@
 import frappe
-from frappe.model.document import Document
+from accounting_app.accounting_app.controllers.accounting_controller import AccountingController
 
-class JournalEntry(Document):
+class JournalEntry(AccountingController):
+
     def validate(self):
+        self.calculate_and_validate_totals()
+
+    def make_gl_entries(self):
+        for entry in self.get("accounting_entries"):
+            if entry.debit or entry.credit:
+                self._create_gl_entry(
+                    posting_date=self.posting_date,
+                    account=entry.account,
+                    party=entry.party,
+                    debit=entry.debit,
+                    credit=entry.credit,
+                    remark=entry.description_of_transaction
+                )
+
+    def calculate_and_validate_totals(self):
         total_debit = 0
         total_credit = 0
         for entry in self.get("accounting_entries"):
@@ -13,4 +29,4 @@ class JournalEntry(Document):
         self.total_credit = total_credit
 
         if self.total_debit != self.total_credit:
-            frappe.throw("Total Debit must equal Total Credit.")
+            frappe.throw("Total debit should equal total credit !!!!!!")
