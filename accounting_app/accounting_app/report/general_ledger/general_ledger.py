4ab7b979e4c405fecbddc4ce8734c5ff90985b86
Abdul-AMA <abdamapro22@gmail.com>
2025-06-18 18:06:36 +0300

    included also all gls "cancelled and not"

--- accounting_app/accounting_app/report/general_ledger/general_ledger.py
+++ accounting_app/accounting_app/report/general_ledger/general_ledger.py
@@ -1,5 +1,4 @@
 import frappe
-from frappe.query_builder.functions import Sum
 
 def execute(filters):
     columns = get_columns()
@@ -10,9 +9,8 @@ def get_columns():
     return [
         {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 100},
         {"label": "Account", "fieldname": "account", "fieldtype": "Link", "options": "Account", "width": 200},
-        {"label": "Debit", "fieldname": "debit_amount", "fieldtype": "Currency", "width": 120},
-        {"label": "Credit", "fieldname": "credit_amount", "fieldtype": "Currency", "width": 120},
-        {"label": "Balance", "fieldname": "balance", "fieldtype": "Currency", "width": 120},
+        {"label": "Debit", "fieldname": "debit_amount", "fieldtype": "Currency", "width": 150},
+        {"label": "Credit", "fieldname": "credit_amount", "fieldtype": "Currency", "width": 150},
         {"label": "Voucher Type", "fieldname": "voucher_type", "width": 120},
         {"label": "Voucher No", "fieldname": "voucher_number", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 150},
         {"label": "Party", "fieldname": "party", "fieldtype": "Link", "options": "Party", "width": 150},
@@ -21,24 +19,12 @@ def get_columns():
 
 def get_data(filters):
     gl_entry = frappe.qb.DocType("GL Entry")
-    
-    conditions = []
 
-    if filters.get("from_date"):
-        conditions.append(gl_entry.posting_date >= filters.get("from_date"))
-    if filters.get("to_date"):
-        conditions.append(gl_entry.posting_date <= filters.get("to_date"))
-    if filters.get("account"):
-        conditions.append(gl_entry.account == filters.get("account"))
-    if filters.get("party"):
-        conditions.append(gl_entry.party == filters.get("party"))
-
-    data = (
+    query = (
         frappe.qb.from_(gl_entry)
         .select(gl_entry.star)
-        .where(conditions) 
         .orderby(gl_entry.posting_date, gl_entry.creation)
-    ).run(as_dict=True)
+    )
 
     if filters.get("from_date"):
         query = query.where(gl_entry.posting_date >= filters.get("from_date"))
