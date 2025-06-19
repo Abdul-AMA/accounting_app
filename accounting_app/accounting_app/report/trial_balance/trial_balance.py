732cd40772e5ad4f78b39f61d542c8b0f23031b6
Abdulrahman Ammouna <abdamapro22@gmail.com>
2025-06-19 16:23:38 +0300

    Feature/report trail balance (#30)
    * report: trail balance report shows closing balances of all accounts
    
    * build: new version
    
    * refactor: update trial balance report to minimize accessing the db
    
    * fix: guess what now you cann't click on it
    
    * fix: fixed total issue
    
    * refacrot: fixed summery format

---
+++ accounting_app/accounting_app/report/trial_balance/trial_balance.py
@@ -0,0 +1,80 @@
+import frappe
+from frappe.query_builder.functions import Sum
+
+def execute(filters):
+    columns = get_columns()
+    data, total_debit, total_credit = get_data(filters)
+    summary = get_report_summary(total_debit, total_credit)
+    return columns, data, None,None, summary
+
+def get_columns():
+    return [
+        {"label": "Account", "fieldname": "account", "fieldtype": "Link", "options": "Account", "width": 400},
+        {"label": "Debit", "fieldname": "debit", "fieldtype": "Currency", "width": 150},
+        {"label": "Credit", "fieldname": "credit", "fieldtype": "Currency", "width": 150},
+    ]
+
+def get_data(filters):
+    accounts = frappe.get_all("Account", filters={"is_group": 0}, fields=["name"])
+    
+    data = []
+    total_debit = 0
+    total_credit = 0
+
+    as_on_date = filters.get("as_on_date")
+
+    gl_entry = frappe.qb.DocType("GL Entry")
+    
+    all_balances = (
+        frappe.qb.from_(gl_entry)
+        .select(
+            gl_entry.account,
+            (Sum(gl_entry.debit_amount) - Sum(gl_entry.credit_amount)).as_("balance")
+            )
+        .where(
+            (gl_entry.posting_date <= as_on_date)
+        )
+        .groupby(gl_entry.account)
+    ).run(as_dict=True)
+
+    balances_dict = {d.account: d.balance for d in all_balances}
+
+
+    for acc in accounts:
+        account_name = acc.get("name")
+        
+        balance = balances_dict.get(account_name, 0)
+
+        row = {"account": account_name}
+        if balance >= 0:  
+            row["debit"] = balance
+            row["credit"] = 0
+            total_debit += balance
+        else:
+            row["debit"] = 0
+            row["credit"] = abs(balance) 
+            total_credit += abs(balance)
+        
+        data.append(row)
+
+    return data, total_debit, total_credit
+
+
+def get_report_summary(total_debit, total_credit):
+    return [
+        {
+            "label": "Total Debit",
+            "value": total_debit,
+            "indicator": "Green",
+            "datatype": "Currency"
+            
+        },
+        {
+            "label": "Total Credit",
+            "value": total_credit,
+            "indicator": "Red",
+            "datatype": "Currency"
+            
+        }
+    ]
+
