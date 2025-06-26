76c408cc088699f3d5e24bed10f8ecd2ebc32ed5
Abdulrahman Ammouna <abdamapro22@gmail.com>
2025-06-26 18:05:38 +0300

    Feature/inventory sales invoice (#39)
    * refactor: update SI structure to fit the new design
    
    * updated si ui to fetch accounts when is stock
    
    * refaactor: added cogs account in warehouse
    
    * feat: implement GL and stock entries for Sales Invoice
    
    - Adds GL entries on submission: debits customer, credits income,
      and handles COGS and stock accounts grouped by account.
    - Adds stock ledger entries for stock items on submit and cancel.
    
    * build: new version
    
    * update naming
    
    * refactor: avoid DB calls by using is_stock field in Sales Invoice

--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.js
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.js
@@ -20,6 +20,18 @@ frappe.ui.form.on('Sales Invoice', {
             };
         });
 
+        frm.set_query('expense_account', 'items', () => {
+            return {
+                filters: { 'account_type': 'Expense', 'is_group': 0 }
+            };
+        });
+
+        frm.set_query('stock_account', 'items', () => {
+            return {
+                filters: { 'account_type': 'Asset', 'is_group': 0 }
+            };
+        });
+
    },
 
     calculate_totals: function(frm) {
@@ -37,7 +49,28 @@ frappe.ui.form.on('Sales Invoice', {
         frm.set_value('total_amount', total_amount);
         frm.refresh_field('total_qty');
         frm.refresh_field('total_amount');
+    },
+
+    warehouse: async function(frm) {
+        if (!frm.doc.warehouse || !frm.doc.items) return;
+        const { message } = await frappe.db.get_value('Warehouse', frm.doc.warehouse, ['account', 'expense_account']);
+        const warehouse_account = message?.account;
+        const warehouse_expense_account = message?.expense_account;
+        for (const row of frm.doc.items) {
+            if (row.is_stock) {
+                frappe.model.set_value(row.doctype, row.name, 'warehouse', frm.doc.warehouse);
+                if (warehouse_account) {
+                    frappe.model.set_value(row.doctype, row.name, 'stock_account', warehouse_account);
+                }
+                if (warehouse_expense_account) {
+                    frappe.model.set_value(row.doctype, row.name, 'expense_account', warehouse_expense_account);
+                }
+            } else {
+                frappe.model.set_value(row.doctype, row.name, 'warehouse', '');
+            }
+        }
     }
+
 });
 
 frappe.ui.form.on('Sales Invoice Item', {
@@ -56,5 +89,40 @@ frappe.ui.form.on('Sales Invoice Item', {
     },
     items_remove: function(frm) {
         frm.events.calculate_totals(frm);
+    },
+    item: function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+
+        if (!row.item) return;
+        frappe.db.get_value('Item', row.item, 'maintain_stock', async (r) => {
+            const is_stock_item = !!r.maintain_stock;
+
+            frappe.model.set_value(cdt, cdn, 'is_stock_item', is_stock_item ? 1 : 0);
+            if (is_stock_item) {
+                const { message } = await frappe.db.get_value('Warehouse', frm.doc.warehouse, ['account', 'expense_account']);
+                const warehouse_account = message?.account;
+                const warehouse_expense_account = message?.expense_account;
+                frappe.model.set_value(cdt, cdn, 'stock_account', warehouse_account);
+                frappe.model.set_value(cdt, cdn, 'expense_account', warehouse_expense_account); 
+                frappe.model.set_value(cdt, cdn, 'warehouse', frm.doc.warehouse);
+            }else {
+                frappe.model.set_value(cdt, cdn, 'warehouse', '');
+            }
+            const grid_row = frm.fields_dict.items.grid.get_row(cdn);
+            if (grid_row) {
+                grid_row.toggle_display('warehouse', is_stock_item);
+                grid_row.toggle_display('stock_account', is_stock_item);
+                grid_row.toggle_display('expense_account', is_stock_item);
+            }
+        });
+    },
+    warehouse: async function(frm, cdt, cdn) {
+        const row = locals[cdt][cdn];
+        if (!row.warehouse) return;
+        const { message } = await frappe.db.get_value('Warehouse', row.warehouse, 'account');
+        const warehouse_account = message?.account;
+        if (warehouse_account) {
+            frappe.model.set_value(cdt, cdn, 'stock_account', warehouse_account);
+        }
     }
 });
--- accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
+++ accounting_app/accounting_app/doctype/sales_invoice/sales_invoice.py
@@ -1,3 +1,4 @@
+import frappe
 from ...controllers.AccountingController import AccountingController
 
 class SalesInvoice(AccountingController):
@@ -5,26 +6,14 @@ class SalesInvoice(AccountingController):
     def before_save(self):
         self.calculate_totals()
 
-    def make_gl_entries(self):
+    def on_submit(self):
+        self.make_gl_entries()
+        self.make_stock_ledger_entries()
 
-        #debit entry
-        self._create_gl_entry(
-            posting_date=self.posting_date,
-            account=self.debit_to,
-            party=self.customer,
-            debit=self.total_amount,
-            credit=0,
-            due_date=self.payment_due_date
-        )
+    def on_cancel(self):
+        super().on_cancel()
+        self.make_reverse_stock_ledger_entries()
 
-        #credit entry
-        self._create_gl_entry(
-            posting_date=self.posting_date,
-            account=self.income_account,
-            party=self.customer,
-            debit=0,
-            credit=self.total_amount
-        )
 
     def calculate_totals(self):
         total_qty = 0
@@ -34,3 +23,83 @@ class SalesInvoice(AccountingController):
             total_amount += float(item_row.amount or 0)
         self.total_qty = total_qty
         self.total_amount = total_amount
+
+    def make_gl_entries(self):
+        self._create_gl_entry(
+            posting_date=self.posting_date,
+            account=self.debit_to,
+            party=self.customer,
+            debit=self.total_amount,
+            credit=0
+        )
+
+        self._create_gl_entry(
+            posting_date=self.posting_date,
+            account=self.income_account,
+            party=self.customer,
+            due_date=self.payment_due_date,
+            debit=0,
+            credit=self.total_amount
+        )
+
+        expense_totals = {}
+        stock_totals = {}
+
+        for item in self.get("items"):
+            if item.is_stock:
+                if item.expense_account:
+                    expense_totals.setdefault(item.expense_account, 0)
+                    expense_totals[item.expense_account] += float(item.amount or 0)
+
+                if item.stock_account:
+                    stock_totals.setdefault(item.stock_account, 0)
+                    stock_totals[item.stock_account] += float(item.amount or 0)
+
+        for account, amount in expense_totals.items():
+            self._create_gl_entry(
+                posting_date=self.posting_date,
+                account=account,
+                party=self.customer,
+                due_date=self.payment_due_date,
+                debit=amount,
+                credit=0
+            )
+
+        for account, amount in stock_totals.items():
+            self._create_gl_entry(
+                posting_date=self.posting_date,
+                account=account,
+                party=self.customer,
+                due_date=self.payment_due_date,
+                debit=0,
+                credit=amount
+            )
+
+    def make_stock_ledger_entries(self):
+        for item in self.get("items"):
+            if item.is_stock:
+                sle = frappe.new_doc("Stock Ledger Entry")
+                sle.item = item.item
+                sle.warehouse = item.warehouse
+                sle.posting_date = self.posting_date
+                sle.actual_qty = -item.qty 
+                sle.valuation_rate = item.rate
+                sle.voucher_type = self.doctype
+                sle.voucher_number = self.name
+                sle.insert(ignore_permissions=True)
+                sle.submit()
+
+    def make_reverse_stock_ledger_entries(self):
+        for item in self.get("items"):
+            if item.is_stock:
+                sle = frappe.new_doc("Stock Ledger Entry")
+                sle.item = item.item
+                sle.warehouse = item.warehouse
+                sle.posting_date = self.posting_date
+                sle.actual_qty = item.qty  
+                sle.valuation_rate = item.rate
+                sle.voucher_type = self.doctype
+                sle.voucher_number = self.name
+                sle.remark = f"Reversal for cancellation of {self.name}"
+                sle.insert(ignore_permissions=True)
+                sle.submit()
